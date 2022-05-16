09c403b15763bf401c3cf1157711771d83fa9e14
Nick <nikkomunoz@gmail.com>
2022-05-16 13:04:30 -0700

    added priveldges to admin

--- web/src/init_db.py
+++ web/src/init_db.py
@@ -41,6 +41,8 @@ root_cursor = root_db.cursor()
 
 # create database
 root_cursor.execute(f"CREATE DATABASE IF NOT EXISTS {db_name};")
+
+root_cursor.execute(f"CREATE USER '{db_user}'@'{db_host}' IDENTIFIED BY '{db_user_pass}';")
 root_cursor.execute(f"USE {db_name}")
 
 # # CAUTION!!! CAUTION!!! CAUTION!!! CAUTION!!! CAUTION!!! CAUTION!!! CAUTION!!!
@@ -62,8 +64,8 @@ tables.create_tables(root_cursor)
 root_db.commit()
 
 # grant privileges to user
-# for table in vip_tableList:
-#   root_cursor.execute(f"GRANT INSERT, UPDATE, DELETE, SELECT, REFERENCES ON {db_name}.{table} TO '{db_vip}'@'{db_host}'")
+for table in complete_tablesList:
+  root_cursor.execute(f"GRANT INSERT, UPDATE, DELETE, SELECT, REFERENCES ON {db_name}.{table} TO '{db_user}'@'{db_host}'")
 # # grant privileges to customer
 # for table in customers_tableList:
 #   root_cursor.execute(f"GRANT INSERT, UPDATE, DELETE, SELECT, REFERENCES ON {db_name}.{table} TO '{db_customer}'@'{db_host}'")
