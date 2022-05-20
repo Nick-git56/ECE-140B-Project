2b3d26550ec50e96c2e0b9d941b602511ca2decd
Nick <nikkomunoz@gmail.com>
2022-05-20 14:17:22 -0700

    updated Node network system

--- hardware/src/simulated-socket.py
+++ hardware/src/simulated-socket.py
@@ -4,6 +4,48 @@ import matplotlib.pyplot as plt
 
 node_types = ["tag","anchor","gateway"]
 
+
+
+class NetworkGod:
+    """Primary interface for interacting with nodes and networks"""
+    def __init__(self):
+        self.network_list = []
+    
+    def addNetwork(self,net):
+        self.network_list.append(net)
+
+class Node:
+    def __init__(self,type,id,name):
+        self.name = name
+        self.node_type = type
+        self.node_id = id
+
+class Network:
+    """Primary interface for 
+    """
+    def __init__(self,types):
+        self.available_types = types
+        self.node_count_by_type = dict(zip(node_types, [0]*len(node_types)))
+        self.node_list = []
+    
+    def addNode(self,type,id,name):
+        assert type in self.available_types
+
+        self.node_list.append(Node(type,id,name))
+        # update counter dict
+        #check if exists...
+        #self.node_count_by_type[node.type] = self.node_count_by_type[node.type]+1
+
+    def getNodeCount(self,type):
+        # get count of nodes based on type
+
+    def updateNodePosition(NodeName):
+        # look for node with this name
+            # update its position
+
+
+
+
 class Network:
     def __init__(self):
         # KV pair = Type : count
@@ -91,25 +133,25 @@ tags_list = ["TI:PS:LA:LA:PO:EH",
             "ZI:AD:LA:LA:PO:EL",
             "ZI:PS:GG:LA:PO:EL"]
 
-if __name__ == "__main__":    
-    simulated_tags = {tags_list[idx]: SimulatedNode('tag',tags_list[idx],name=f'tag{idx}') for idx in range(len(tags_list))}
-    detected_tags = {}
+# if __name__ == "__main__":    
+#     simulated_tags = {tags_list[idx]: SimulatedNode('tag',tags_list[idx],name=f'tag{idx}') for idx in range(len(tags_list))}
+#     detected_tags = {}
     
-    for _ in range(2000):
-        received_id = numpy.random.choice(tags_list)
-        received_position = simulated_tags[received_id].get_random_position_updates()
-        received_packet = {"tag_ID": received_id, "position_data": received_position}
-        if received_packet["tag_ID"] not in detected_tags.keys():
-            detected_tags[received_packet["tag_ID"]] = Node("tag", received_packet["tag_ID"], name=received_packet["tag_ID"])
-        detected_tags[received_packet["tag_ID"]].update_position(received_position)        
+#     for _ in range(2000):
+#         received_id = numpy.random.choice(tags_list)
+#         received_position = simulated_tags[received_id].get_random_position_updates()
+#         received_packet = {"tag_ID": received_id, "position_data": received_position}
+#         if received_packet["tag_ID"] not in detected_tags.keys():
+#             detected_tags[received_packet["tag_ID"]] = Node("tag", received_packet["tag_ID"], name=received_packet["tag_ID"])
+#         detected_tags[received_packet["tag_ID"]].update_position(received_position)        
     
     
-    for tag in detected_tags.values():
-        x,y = get_position_history(tag,get_just_values=True)
-        plt.plot(x,y)
-    plt.show()
+#     for tag in detected_tags.values():
+#         x,y = get_position_history(tag,get_just_values=True)
+#         plt.plot(x,y)
+#     plt.show()
     
-    for tag in simulated_tags.values():
-        x,y = get_position_history(tag,get_just_values=True)
-        plt.plot(x,y)
-    plt.show()
+#     for tag in simulated_tags.values():
+#         x,y = get_position_history(tag,get_just_values=True)
+#         plt.plot(x,y)
+#     plt.show()
